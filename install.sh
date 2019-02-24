d122b4c78d0d19f71852622cd2b21529ab53a67f
Jeffrey Yang <jeffreyyang@Jeffreys-MacBook-Air.local>
2019-02-24 00:55:21 -0800

    script

---
+++ install.sh
@@ -0,0 +1,20 @@
+cd ~/.vim/bundle/
+declare -a links=(
+"https://github.com/jiangmiao/auto-pairs.git"
+
+"https://github.com/mattn/emmet-vim.git"
+"https://github.com/junegunn/fzf.vim.git"
+"https://github.com/othree/html5.vim.git"
+"https://github.com/scrooloose/nerdtree.git"
+"https://github.com/vim-airline/vim-airline.git"
+"https://github.com/vim-airline/vim-airline-themes"
+"https://github.com/tpope/vim-fugitive.git"
+"https://github.com/pangloss/vim-javascript.git"
+"https://github.com/posva/vim-vue.git"
+"https://github.com/mxw/vim-jsx.git"
+) 
+
+for i in "${links[@]}"
+do
+    git clone "$i" 
+done
